c1d4be20709d439bcb35ac68360c85c280aa54fb
AmaranthYan <amaranth.yan@gmail.com>
2019-05-06 01:47:36 +0800

    Code overlap compute shader, fix some editor glitches and add overlap visualization

--- Assets/Editor/OctreeEditor.cs
+++ Assets/Editor/OctreeEditor.cs
@@ -22,12 +22,15 @@ namespace NavVolume.Editor
         ThreadPool m_ThreadPool;
         private void OnEnable()
         {
+            SceneView.onSceneGUIDelegate += OnSceneGUI;
+
             m_ThreadPool = new ThreadPool(8);
             
             m_OverlapShader = AssetDatabase.LoadAssetAtPath<ComputeShader>(OVERLAP_SHADER_PATH);
             if (m_OverlapShader)
             {
-                m_Kernel = m_OverlapShader.FindKernel("Overlap");
+                m_ComputeKernel = m_OverlapShader.FindKernel("Compute");
+                m_ResetKernel = m_OverlapShader.FindKernel("Reset");
             }
             else
             {
@@ -41,6 +44,7 @@ namespace NavVolume.Editor
         private void OnDestroy()
         {
             m_ThreadPool.Dispose();
+            SceneView.onSceneGUIDelegate -= OnSceneGUI;
         }
         
         [MenuItem("NavVolume/Octree Editor")]
@@ -63,12 +67,9 @@ namespace NavVolume.Editor
 
             GUI.enabled = enabled;
         }
-
-        private void OnGUI()
+        
+        private void Update()
         {
-            m_IsProcessing = false;
-
-            string state = string.Empty;
             switch (m_State)
             {
                 case State.None:
@@ -81,11 +82,13 @@ namespace NavVolume.Editor
                         if (m_BatchCount == m_BatchDone)
                         {
                             m_State = State.Extracted;
+                            m_IsProcessing = false;
+
+                            Repaint();
                         }
                         else
                         {
                             state = "extracting triangles";
-                            m_IsProcessing = true;
                         }
                     }
                     else
@@ -100,15 +103,68 @@ namespace NavVolume.Editor
                     break;
 
             }
-            
-            
+        }
+
+        string state = string.Empty;
+        private void OnGUI()
+        {   
             GUILayout.Label(state);
             
             GUIStateButton(State.Initialized, "Extract Obstacle Triangles", ExtractObstacleTriangles);
             GUIStateButton(State.Extracted, "Generate Octree Data", GenerateOctreeData);
             GUIStateButton(State.Generated, "Construct NavVolume", () => { });            
         }
-        
+
+        private void OnSceneGUI(SceneView sceneView)
+        {
+            Vector3 cube = new Vector3(1, 1, 1);
+
+            Vector3 root = Vector3.zero;
+            float edge = 4;
+
+            int sum = 0;
+            for (int i = 0; i < 64; i++)
+            {
+                int k = 0;
+
+                int c = i;
+                var sub_root = root;
+                int p = 8;
+                do
+                {
+                    int sub = c % p;
+                    c = c / p;
+
+                    var offset = new Vector3(c & 1, (c / 2) & 1, (c / 4) & 1);
+                    offset = 2 * offset - Vector3.one;
+                    sub_root += edge / (1 << (k + 1)) / 2 * offset;
+
+                    c = sub;
+                    p /= 8;
+                } while (++k < 2);
+
+                Handles.zTest = UnityEngine.Rendering.CompareFunction.Less;
+                Handles.color = Color.white;
+                if (data[i] > 0)
+                {
+                    Handles.color = Color.red;
+                    Handles.DrawWireCube(sub_root, cube);
+                }
+                sum += data[i];
+                //if (r2[i] > 0)
+                //{
+                //    Gizmos.color = Color.red;
+                //    Gizmos.DrawCube(r[i], cube);
+                //}
+                //else
+                //{
+                //    Gizmos.color = Color.white;
+                //    Gizmos.DrawWireCube(r[i], cube);
+                //}
+            }
+        }
+
+
         private const int TRIANGLES_PER_BATCH = 10000;
         List<ObstacleTriangle> m_ObstacleTriangles = new List<ObstacleTriangle>();
         int m_BatchCount = 0;
@@ -169,23 +225,47 @@ namespace NavVolume.Editor
                     Debug.LogWarning("Obstacle without mesh");
                 }
             }
+
+            if (m_BatchCount > 0)
+            {
+                m_IsProcessing = true;
+            }
         }
+        
+        int[] data = new int[64];
 
         private const string OVERLAP_SHADER_PATH = "Assets/CShader/CubeTriangleOverlap.compute";
         private ComputeShader m_OverlapShader;
-        private int m_Kernel;
+        private int m_ComputeKernel;
+        private int m_ResetKernel;
         private void GenerateOctreeData()
         {
             var triangles = new ComputeBuffer(m_ObstacleTriangles.Count, sizeof(float) * 45);
             triangles.SetData(m_ObstacleTriangles);
-            m_OverlapShader.SetBuffer(m_Kernel, "Input", triangles);
-            
-            //m_ComputeShader.SetBuffer(kernel, "intersection", buffer2);
+            m_OverlapShader.SetBuffer(m_ComputeKernel, "input", triangles);
+
+            var octree = new ComputeBuffer(64, sizeof(int), ComputeBufferType.Raw);
+            m_OverlapShader.SetBuffer(m_ComputeKernel, "output", octree);
+
+            // reset buffer
+            m_OverlapShader.SetBuffer(m_ResetKernel, "output", octree);
+            m_OverlapShader.Dispatch(m_ResetKernel, 1, 1, 1);
 
             m_OverlapShader.SetFloats("center", new float[3] { 0, 0, 0 });
             m_OverlapShader.SetFloat("half_edge", 0.5f);
 
+            m_OverlapShader.Dispatch(m_ComputeKernel, m_ObstacleTriangles.Count, 1, 1);
+            octree.GetData(data);
+
+            foreach (var d in data)
+            {
+                Debug.Log(d);
+            }
+
+            octree.Release();
             triangles.Release();
+            
+            SceneView.RepaintAll();
         }
     }
 }
